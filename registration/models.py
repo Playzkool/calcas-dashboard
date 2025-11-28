177daa1c3e541ac37d1153c417e2465380435c57
Sébastien Terrebonne <sebastien.terrebonne@ze-watt.com>
2025-11-28 14:33:05 +0100

    Continued tu structure project

--- registration/models.py
+++ registration/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class LegalRepresentative(models.Model):
+    user = models.OneToOneField(User, verbose_name="User", on_delete=models.CASCADE)
+    date_creation = models.DateTimeField(auto_now_add=True)
+    parameters = models.JSONField(verbose_name="Parametres", default=dict, editable=True, blank=True)
+
