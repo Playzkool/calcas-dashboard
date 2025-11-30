dd2b91251343e9c3b87ca0e3e3c378b2fcca65b8
Sébastien Terrebonne <sebastien.terrebonne@ze-watt.com>
2025-11-30 22:21:30 +0100

    Login WIP

--- registration/views.py
+++ registration/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
 
 # Create your views here.
+class LoginViewHome(LoginView):
+    template_name = 'registration/login.html'
+    login_url = settings.LOGIN_URL
+    redirect_field_name = settings.LOGIN_REDIRECT_URL
